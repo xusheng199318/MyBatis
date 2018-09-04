a9258097604231c706a74aa0eeb7335151b9b5ef
Arthur <arthur@qq.com>
2018-09-04 13:29:55 +0800

    MyBatis基础应用，最原始的MyBatis使用方式

---
+++ src/main/java/com/arthur/pojo/User.java
@@ -0,0 +1,90 @@
+package com.arthur.pojo;
+
+import java.util.Date;
+
+/**
+ * Created by xusheng on 2018/9/4.
+ */
+public class User {
+
+    private int id;
+    private String username;
+    private Date birthday;
+    private String sex;
+    private String address;
+
+    public User id(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public User username(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public User birthday(Date birthday) {
+        this.birthday = birthday;
+        return this;
+    }
+
+    public User sex(String sex) {
+        this.sex = sex;
+        return this;
+    }
+
+    public User address(String address) {
+        this.address = address;
+        return this;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", birthday=" + birthday +
+                ", sex='" + sex + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/arthur/mybatis/MyBatisTest.java
@@ -0,0 +1,83 @@
+package com.arthur.mybatis;
+
+import com.arthur.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by xusheng on 2018/9/4.
+ * MyBatis基本应用
+ * MyBatis最原始的使用方式，
+ * 通过SqlSessionFactoryBuilder构建SqlSessionFactory
+ * 从SqlSessionFactory中获取一个SqlSession
+ *
+ */
+public class MyBatisTest {
+
+    private SqlSessionFactory sqlSessionFactory;
+    private SqlSession sqlSession;
+
+    @Before
+    public void init() throws IOException {
+        String resource = "SqlMapConfig.xml";
+        InputStream is = Resources.getResourceAsStream(resource);
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+    }
+
+    @Test
+    public void testSelectOne() {
+        User user = sqlSession.selectOne("test.findUserById", 1);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testSelectList() {
+        List<User> users  = sqlSession.selectList("findByUserName", "张");
+        System.out.println(users);
+    }
+
+    @Test
+    public void testInsert() {
+        User user = new User()
+                .username("test03")
+                .sex("男")
+                .birthday(new Date())
+                .address("上海");
+
+        System.out.println(user);
+        sqlSession.insert("test.insertUser", user);
+        System.out.println(user);
+        sqlSession.commit();
+        System.out.println(user);
+    }
+
+    @Test
+    public void testDelete() {
+        sqlSession.delete("test.deleteUserById", 39);
+        sqlSession.commit();
+    }
+
+    @Test
+    public void testUpdate() {
+        User user = new User().id(38).username("test03");
+        sqlSession.update("test.updateUserById", user);
+        sqlSession.commit();
+    }
+
+    @After
+    public void destroy() {
+        if (sqlSession != null) {
+            sqlSession.close();
+        }
+    }
+}
